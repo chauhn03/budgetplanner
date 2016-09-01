6176d7cae0a61c786f41cbbe96cf0fa2d9ff7342
chau.hoang <chauhn.it@gmail.com>
2016-09-01 09:19:52 +0700

    QLCT-21: specify test

--- test/spec/controllers/about.js
+++ test/spec/controllers/about.js
@@ -1,31 +1,31 @@
-//'use strict';
-//
-//describe('example test', function() {
-//  it('should be true', function() {
-//    expect('abc').toBe('abc');
-//  });
-//});
-
 'use strict';
 
-describe('Controller: AboutCtrl', function () {
-
-  // load the controller's module
-  beforeEach(module('budgetplannerApp'));
-
-  var AboutCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    AboutCtrl = $controller('AboutCtrl', {
-      $scope: scope
-      // place here mocked dependencies
-    });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(AboutCtrl.awesomeThings.length).toBe(3);
+describe('example test', function() {
+  it('should be true', function() {
+    expect('abc').toBe('abc');
   });
 });
+
+//'use strict';
+//
+//describe('Controller: AboutCtrl', function () {
+//
+//  // load the controller's module
+//  beforeEach(module('budgetplannerApp'));
+//
+//  var AboutCtrl,
+//    scope;
+//
+//  // Initialize the controller and a mock scope
+//  beforeEach(inject(function ($controller, $rootScope) {
+//    scope = $rootScope.$new();
+//    AboutCtrl = $controller('AboutCtrl', {
+//      $scope: scope
+//      // place here mocked dependencies
+//    });
+//  }));
+//
+//  it('should attach a list of awesomeThings to the scope', function () {
+//    expect(AboutCtrl.awesomeThings.length).toBe(3);
+//  });
+//});
