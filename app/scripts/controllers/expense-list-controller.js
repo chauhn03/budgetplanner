ec89c26356d306f5216c323cd968d982a487d857
chau.hoang <chauhn.it@gmail.com>
2016-09-06 22:20:07 +0700

    bind data to tree view

--- app/scripts/controllers/expense-list-controller.js
+++ app/scripts/controllers/expense-list-controller.js
@@ -1,6 +1,29 @@
-app.controller('ExpenseListController', function ($scope, $timeout) {
-    $scope.text = "Expense List";   
+app.controller('ExpenseListController', function ($scope) {
+    $scope.text = "Expense List";
+    $scope.vm = this;
+    $scope.incomes = [
+        {Id: 1, Name: "Lương"},
+        {Id: 2, Name: "Phụ cấp"},
+        {Id: 3, Name: "Thưởng"}
+    ];
+    $scope.expenseGroups = [
+        {
+            Id: 1,
+            Group: "Chi phí đi lại",
+            Expense: [{
+                    Id: 1,
+                    Name: "Xang"
+                }]},
+        {
+            Id: 2,
+            Group: "Chi phí đi lại",
+            Expense: [{
+                    Id: 4,
+                    Name: "Xang"
+                }]}
+    ];
+
     $('.tree-toggle').click(function () {
-	$(this).parent().children('ul.tree').toggle(200);
-});
+        $(this).parent().children('ul.tree').toggle(200);
+    });
 });
